7c30d2a3131ed554b359be06ef23e3bdff60e61f
Shogo Kawaguchi <platycod0n.ramosa@gmail.com>
2013-09-28 16:53:46 +0900

    rails初期化 & scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsAjax::Application.config.secret_key_base = 'ba5707db644ee9495f571c63ead540fbe70da39b1a9e9a80ce85cf45651f734ef9b4a305af82f042c8800fc9f099b1ab7c7886c2bec7f85d2bad1dd5d754d558'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsAjax::Application.config.session_store :cookie_store, key: '_rails-ajax_session'
